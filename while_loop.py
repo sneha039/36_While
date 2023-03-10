e3f6f74bcb560ba7ac2e2636e8ecb3511d65f584
sneha039 <123440006+sneha039@users.noreply.github.com>
2023-03-10 14:22:13 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,17 @@
+# i=int(input('Enter any Start Value:'))
+# k=int(input('Enter the End Value:'))
+# while(i<=k):
+#     print(i)
+#     i+=1
+
+# i=int(input('Enter any Start Value:'))
+# k=int(input('Enter the End Value:'))
+# while(i>=k):
+#     print(i)
+#     i+=1
+
+# i=1
+# while(i<=10):
+#     print('Hi',i)
+#     i+=1
+
